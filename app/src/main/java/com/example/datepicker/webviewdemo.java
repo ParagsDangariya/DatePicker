91707802825688c3d3bd93151e2f9b41cb46e6bc
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2019-08-22 16:23:02 -0400

    web view

---
+++ app/src/main/java/com/example/datepicker/webviewdemo.java
@@ -0,0 +1,39 @@
+package com.example.datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class webviewdemo extends AppCompatActivity {
+
+
+    EditText txt;
+    Button btn;
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webviewdemo);
+
+        txt =findViewById(R.id.edt_txt);
+        btn = findViewById(R.id.btn);
+        webview = findViewById(R.id.web);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = txt.getText().toString();
+
+                webview.getSettings().setLoadsImagesAutomatically(true);
+                webview.getSettings().setJavaScriptEnabled(true);
+                webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                webview.loadUrl(url);
+            }
+        });
+    }
+}
